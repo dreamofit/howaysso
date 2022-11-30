166eee100c796b494637d9c2b88b620e97e1f8fe
howay <358619371@qq.com>
2022-11-30 14:10:39 +0800

    通过所在配置相对地址进行读取

--- howaysso-util/src/main/java/cn/ihoway/util/HowayLog.java
+++ howaysso-util/src/main/java/cn/ihoway/util/HowayLog.java
@@ -2,9 +2,10 @@ package cn.ihoway.util;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
+import org.springframework.core.io.ClassPathResource;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 /**
@@ -14,18 +15,18 @@ public class HowayLog {
     private static final Logger logger = Logger.getLogger(HowayLog.class);
     private Class clazz = HowayLog.class;
     public static void initLog() {
-        FileInputStream fileInputStream = null;
+        InputStream inputStream = null;
         try {
             Properties properties = new Properties();
-            fileInputStream = new FileInputStream("C:/github/howaysso/howaysso-util/src/main/resources/log4j.properties");
-            properties.load(fileInputStream);
+            inputStream = new ClassPathResource("log4j.properties").getInputStream();
+            properties.load(inputStream);
             PropertyConfigurator.configure(properties);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            if (fileInputStream != null) {
+            if (inputStream != null) {
                 try {
-                    fileInputStream.close();
+                    inputStream.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -41,25 +42,25 @@ public class HowayLog {
     }
     public static void info(Class clazz,Object message){
         HowayLog.initLog();
-        logger.info("["+clazz.getSimpleName()+"] "+message);
+        logger.info("[" + clazz.getSimpleName()+"] " + message);
     }
     public static void info(Object message,Class clazz){
         HowayLog.initLog();
-        logger.info("["+clazz.getSimpleName()+"] "+message);
+        logger.info("[" + clazz.getSimpleName()+"] "+ message);
     }
     public static void error(Class clazz,Object message){
         HowayLog.initLog();
-        logger.error("["+clazz.getSimpleName()+"] "+message);
+        logger.error("[" + clazz.getSimpleName()+"] " + message);
     }
     public static void error(Object message,Class clazz){
         HowayLog.initLog();
-        logger.error("["+clazz.getSimpleName()+"] "+message);
+        logger.error("[" + clazz.getSimpleName()+"] " + message);
     }
     public void info(Object message){
-        logger.info("["+clazz.getSimpleName()+"] "+message);
+        logger.info("[" + clazz.getSimpleName()+"] " + message);
     }
     public void error(Object message){
-        logger.error("["+clazz.getSimpleName()+"] "+message);
+        logger.error("[" + clazz.getSimpleName()+"] " + message);
     }
 
 }
