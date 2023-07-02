c0bc4791adcdaea7ecf325a400a7c11ebc40cb07
howay <358619371@qq.com>
2023-07-02 17:33:37 +0800

    服务启动完毕说明提示

--- howaysso-access/src/main/java/cn/ihoway/ServiceInit.java
+++ howaysso-access/src/main/java/cn/ihoway/ServiceInit.java
@@ -2,18 +2,22 @@ package cn.ihoway;
 
 import cn.ihoway.util.HowayContainer;
 import cn.ihoway.util.AccessXmlParser;
+import cn.ihoway.util.HowayLog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class ServiceInit {
+
     public static void main( String[] args ) {
+        HowayLog logger = new HowayLog(ServiceInit.class);
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("dubbo/provider.xml");
         context.start();
         HowayContainer container = new HowayContainer();
         container.start();
         AccessXmlParser.init();
         SpringApplication.run(ServiceInit.class, args);
+        logger.info("*** howaysso服务已经启动 ***");
     }
 }
